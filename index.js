c836b601505047099a62a346c6662bc3a097d7e1
Richmond Nkansah <richmondnkansah@gmail.com>
2022-10-15 16:33:50 -0400

    commit tree e7ba1a8a7bbfbee35857e26f781851b91ccbbac3

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+ customerName = 'bob';
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    var BestCustomer = "not bob";
+    return BestCustomer;
+}
+
+function overwriteBestCustomer() {
+
+}
+
+const leastFavoriteCustomer = " ";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Richmond"
+}
